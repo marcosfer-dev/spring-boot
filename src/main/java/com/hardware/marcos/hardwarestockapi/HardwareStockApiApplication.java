e3618e3f8c4ff907c30852c34a22aaf2ef12f7e6
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-05-20 21:10:01 -0300

    feat: cria estrutura inicial do projeto Spring Boot com Maven e PostgreSQL

---
+++ src/main/java/com/hardware/marcos/hardwarestockapi/HardwareStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.hardware.marcos.hardwarestockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareStockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareStockApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hardware/marcos/hardwarestockapi/HardwareStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardware.marcos.hardwarestockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareStockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
