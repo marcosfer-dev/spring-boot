b0421e5daeec673ec3a1307221edd0cf5a820d92
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-05-22 11:14:32 -0300

    feat: cria JPA de produtos

---
+++ src/main/java/com/hardware/marcos/hardwarestockapi/produtos/Produto.java
@@ -0,0 +1,34 @@
+package com.hardware.marcos.hardwarestockapi.produtos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Table(name = "produto")  // Define o nome da tabela no banco de dados
+@Entity(name = "produtos") // Marca a classe como uma entidade JPA e define o nome da entidade
+@Getter // Gera automaticamente os métodos getters (Lombok)
+@Setter // Gera automaticamente os métodos setters (Lombok)
+@AllArgsConstructor // Cria construtor com todos os atributos (Lombok)
+@NoArgsConstructor // Cria construtor vazio (Lombok)
+@EqualsAndHashCode(of = "id") // Compara objetos usando apenas o ID (Lombok)
+public class Produto {
+
+    @Id  // Define o atributo como chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private int quantidade;
+    private String sku;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+}
