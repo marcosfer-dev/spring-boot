b9421019bd9c9dbf3e557711d17b1d36a4562dd1
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-06-24 11:35:15 -0300

    feat: recuperando o token no filtro de segurança

--- src/main/java/com/hardware/marcos/hardwarestockapi/infra/SecurityFilter.java
+++ src/main/java/com/hardware/marcos/hardwarestockapi/infra/SecurityFilter.java
@@ -15,7 +15,20 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response);
 
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token não enviado.");
+        }
+
+        return authorizationHeader;
     }
 }
