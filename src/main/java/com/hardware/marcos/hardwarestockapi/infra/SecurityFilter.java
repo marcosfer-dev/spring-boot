24f6e5ac408042e28d0591c5cba530ffb9130614
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-06-25 11:48:16 -0300

    feat: Autenticando o usuário

--- src/main/java/com/hardware/marcos/hardwarestockapi/infra/SecurityFilter.java
+++ src/main/java/com/hardware/marcos/hardwarestockapi/infra/SecurityFilter.java
@@ -1,10 +1,13 @@
 package com.hardware.marcos.hardwarestockapi.infra;
 
+import com.hardware.marcos.hardwarestockapi.usuarios.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,12 +19,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
 
-        var subject = tokenService.getSubject(tokenJWT);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
 
@@ -30,10 +43,10 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token não enviado.");
+        if (authorizationHeader != null) {
+            return authorizationHeader;
         }
 
-        return authorizationHeader;
+        return null;
     }
 }
